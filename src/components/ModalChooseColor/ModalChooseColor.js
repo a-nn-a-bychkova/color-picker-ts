6a662a809c702e258dcde841221ff04a6bc3d44b
Anna Bychkova <bychkova.dev@gmail.com>
2021-09-01 13:14:21 +0300

    adds colorpicker-slider and logic of color changing

--- src/components/ModalChooseColor/ModalChooseColor.js
+++ src/components/ModalChooseColor/ModalChooseColor.js
@@ -1,25 +1,68 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import style from './ModalChooseColor.module.css';
-import { AlphaPicker, SketchPicker } from 'react-color';
+import { LightnessSlider } from 'react-slider-color-picker';
+import convert from 'color-convert';
 
 function ModalChooseColor() {
-  const [color, setColor] = useState('#ff0000');
-  const [green, setGeen] = useState('#ff0000');
-  const [blue, setBlue] = useState('#ff0000');
-  // const handleChangeComplete = (color, event) => {
-  //   return setColor(color.hex);
-  // };
+  const [redColor, setRedColor] = useState({ h: 0, s: 100, l: 50, a: 1 });
+  const [hexRedColor, setHexRedColor] = useState('ff0000');
+  const [greenColor, setGreenColor] = useState({ h: 120, s: 100, l: 50, a: 1 });
+  const [hexGreenColor, setHexGreenColor] = useState('00ff00');
+  const [blueColor, setBlueColor] = useState({ h: 240, s: 100, l: 50, a: 1 });
+  const [hexBlueColor, setHexBlueColor] = useState('0000ff');
+  const [chosenColor, setChosenColor] = useState();
+
+  function updatedColorToString(newColor) {
+    return [newColor.h, newColor.s, newColor.l];
+  }
+
+  function mixChosenColors() {
+    console.log('hex red mix', hexRedColor);
+    let r = hexRedColor.slice(0, 2);
+    console.log('hex green mix', hexGreenColor);
+    let g = hexGreenColor.slice(2, 4);
+    console.log('hex blue mix', hexBlueColor);
+    let b = hexBlueColor.slice(4);
+    console.log('chosenColor', `${r}${g}${b}`);
+    return setChosenColor(`${r}${g}${b}`);
+  }
+
+  const handleChangeRedColor = newColor => {
+    setRedColor(newColor);
+    let updatedColor = updatedColorToString(newColor);
+    setHexRedColor(convert.hsl.hex(updatedColor));
+    mixChosenColors();
+  };
+  const handleChangeGreenColor = newColor => {
+    setGreenColor(newColor);
+    let updatedColor = updatedColorToString(newColor);
+    setHexGreenColor(convert.hsl.hex(updatedColor));
+    mixChosenColors();
+  };
+  const handleChangeBlueColor = newColor => {
+    setBlueColor(newColor);
+    let updatedColor = updatedColorToString(newColor);
+    setHexBlueColor(convert.hsl.hex(updatedColor));
+    mixChosenColors();
+  };
+
   return (
     <div className={style.Container}>
-      <AlphaPicker
-        color={'#ff0000'}
-        height={'15px'}
-        width={'140px'}
-        onChangeComplete={color => {
-          setColor(color.hex);
-        }}
+      <LightnessSlider
+        handleChangeColor={handleChangeRedColor}
+        color={redColor}
       />
+      <LightnessSlider
+        handleChangeColor={handleChangeGreenColor}
+        color={greenColor}
+      />
+      <LightnessSlider
+        handleChangeColor={handleChangeBlueColor}
+        color={blueColor}
+      />
+      <div></div>
     </div>
   );
 }
+
 export default ModalChooseColor;
